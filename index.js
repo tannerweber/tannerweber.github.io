bbe5192464527738910354b73c9abc6ddbda613c
Tanner Weber <tannerw@pdx.edu>
2025-12-08 19:08:21 -0800

    Moved theme attribute to <html>

--- index.js
+++ index.js
@@ -3,23 +3,36 @@ $(document).ready(() => {
     '<img src=assets/icons/brightness-high.svg style="filter: invert(100%)" alt="light mode icon">';
   const darkIconHTML =
     '<img src=assets/icons/moon-stars-fill.svg style="filter: invert(100%)" alt="dark mode icon">';
+  const themeElement = "html";
 
-  function updateLightDarkMode() {
-    if ($("body").attr("data-bs-theme") == "dark") {
+  function getTheme() {
+    if ($(themeElement).attr("data-bs-theme") === "dark") {
+      return "dark";
+    } else {
+      return "light";
+    }
+  }
+
+  function updateTheme() {
+    if (getTheme() === "dark") {
       $("#dark-light-button").html(darkIconHTML);
     } else {
       $("#dark-light-button").html(lightIconHTML);
     }
   }
 
-  updateLightDarkMode();
+  function flipTheme() {
+    if (getTheme() === "dark") {
+      $(themeElement).attr("data-bs-theme", "light");
+    } else {
+      $(themeElement).attr("data-bs-theme", "dark");
+    }
+  }
 
   $("#dark-light-button").on("click", () => {
-    if ($("body").attr("data-bs-theme") == "dark") {
-      $("body").attr("data-bs-theme", "light");
-    } else {
-      $("body").attr("data-bs-theme", "dark");
-    }
-    updateLightDarkMode();
+    flipTheme();
+    updateTheme();
   });
+
+  updateTheme();
 });
